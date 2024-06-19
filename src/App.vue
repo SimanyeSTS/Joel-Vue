c969993d4fe9ec74b8469c93b10601fb9e220162
SimanyeSTS <simanyesomdaka@gmail.com>
2024-06-19 16:38:19 +0200

    Joel

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form class="container">
+ <input type="text" placeholder="First Name" required v-model="firstName">
+ <input type="text" placeholder="Last Name" required v-model="lastName">
+ <input type="text" placeholder="Gender" required v-model="gender" :class="{
+  male: lowerCaseGender == 'male',
+  female: lowerCaseGender == 'female'
+ }">
+ <textarea name="" id="" cols="30" rows="10"></textarea>
+ <label output></label>
+ <button @click.prevent="userInfo">Display</button>
+</form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+ export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      gender: ''
+    }
+    
+  },
+  computed: {
+    lowerCaseGender() {
+      return this.gender.toLowerCase()
+    }
+  },
+  methods: {
+    userInfo() {
+      document.querySelector('[output]').textContent =
+      `
+      First name: ${this.firstName}
+      Last name: ${this.lastName}
+      Gender: ${this.gender}
+      `
+    }
   }
 }
 </script>
@@ -22,5 +52,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  display: grid;
+  gap: .2rem;
+  width: 30rem;
+  margin-inline: auto;
+  & input { 
+    text-align: center;
+  }
+}
+.male {
+  border: 2px solid blue;
+}
+.female {
+  border: 2px solid pink;
 }
 </style>
